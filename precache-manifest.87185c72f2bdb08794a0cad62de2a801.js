a9c5f5fdc6278df3e5ccceb13bea55cd8381cb7b
Muhammad Abu Bakar <linktobakar@gmail.com>
2019-10-02 00:33:58 +0500

    Updates

---
+++ precache-manifest.87185c72f2bdb08794a0cad62de2a801.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2e099b6728a9942d6ab542a96dcd06",
+    "url": "/React_Vidly_App/index.html"
+  },
+  {
+    "revision": "f43d81b80a1564462534",
+    "url": "/React_Vidly_App/static/css/2.c574a3cb.chunk.css"
+  },
+  {
+    "revision": "aa9440f8aad5d1828d39",
+    "url": "/React_Vidly_App/static/css/main.690c99f9.chunk.css"
+  },
+  {
+    "revision": "f43d81b80a1564462534",
+    "url": "/React_Vidly_App/static/js/2.854b013c.chunk.js"
+  },
+  {
+    "revision": "aa9440f8aad5d1828d39",
+    "url": "/React_Vidly_App/static/js/main.4d0b26e9.chunk.js"
+  },
+  {
+    "revision": "35f5967deba972abf2d5",
+    "url": "/React_Vidly_App/static/js/runtime~main.0174c99d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React_Vidly_App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React_Vidly_App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React_Vidly_App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React_Vidly_App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React_Vidly_App/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
